c8453842671e39fde48b03caadae59956eeec19b
Divy Patel <82171007+divy-07@users.noreply.github.com>
2022-11-22 00:45:23 -0800

    added white pawn tests

--- src/test/java/chess/moves/WPTest.java
+++ src/test/java/chess/moves/WPTest.java
@@ -11,18 +11,193 @@ public class WPTest {
 
     @Test
     public void test01CaptureRight() {
-        // make bitboards
         String[][] stringBoard = new String[][]{
                 {"r", "n", "b", "q", "k", "b", "n", "r"},
+                {"p", "p", "p", "p", "p", "p", " ", " "},
+                {" ", " ", " ", " ", " ", " ", " ", " "},
+                {" ", " ", " ", " ", " ", " ", " ", " "},
+                {" ", " ", " ", " ", " ", " ", " ", " "},
+                {" ", " ", " ", " ", " ", " ", "p", "p"},
+                {" ", " ", " ", " ", " ", " ", "P", " "},
+                {" ", " ", " ", " ", " ", " ", " ", " "}};
+        long[] boardArray = DebugBoard.arrayToBitboards(stringBoard, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L);
+        String expected = "6657";
+        String actual = PossibleMoves.possibleMovesW(boardArray[0], boardArray[1], boardArray[2], boardArray[3],
+                boardArray[4], boardArray[5], boardArray[6], boardArray[7], boardArray[8], boardArray[9],
+                boardArray[10], boardArray[11], 0L, false, false, false, false);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void test02CaptureLeft() {
+        String[][] stringBoard = new String[][]{
+                {"r", "n", "b", "q", "k", "b", "n", "r"},
+                {"p", "p", "p", "p", "p", " ", " ", "p"},
+                {" ", " ", " ", " ", " ", " ", " ", " "},
+                {" ", " ", " ", " ", " ", " ", " ", " "},
+                {" ", " ", " ", " ", " ", " ", " ", " "},
+                {" ", " ", " ", " ", " ", "p", "p", " "},
+                {" ", " ", " ", " ", " ", " ", "P", " "},
+                {" ", " ", " ", " ", " ", " ", " ", " "}};
+        long[] boardArray = DebugBoard.arrayToBitboards(stringBoard, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L);
+        String expected = "6655";
+        String actual = PossibleMoves.possibleMovesW(boardArray[0], boardArray[1], boardArray[2], boardArray[3],
+                boardArray[4], boardArray[5], boardArray[6], boardArray[7], boardArray[8], boardArray[9],
+                boardArray[10], boardArray[11], 0L, false, false, false, false);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void test03MoveOneForward() {
+        String[][] stringBoard = new String[][]{
+                {"r", "n", "b", "q", "k", "b", "n", "r"},
+                {"p", "p", "p", "p", "p", "p", "p", "p"},
+                {" ", " ", " ", " ", " ", " ", " ", " "},
+                {" ", " ", " ", " ", " ", " ", " ", " "},
+                {" ", " ", " ", " ", " ", " ", " ", " "},
+                {" ", " ", " ", " ", " ", " ", "P", " "},
+                {" ", " ", " ", " ", " ", " ", " ", " "},
+                {" ", " ", " ", " ", " ", " ", " ", " "}};
+        long[] boardArray = DebugBoard.arrayToBitboards(stringBoard, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L);
+        String expected = "5646";
+        String actual = PossibleMoves.possibleMovesW(boardArray[0], boardArray[1], boardArray[2], boardArray[3],
+                boardArray[4], boardArray[5], boardArray[6], boardArray[7], boardArray[8], boardArray[9],
+                boardArray[10], boardArray[11], 0L, false, false, false, false);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void test04MoveTwoForward() {
+        String[][] stringBoard = new String[][]{
+                {"r", "n", "b", "q", "k", "b", "n", "r"},
+                {"p", "p", "p", "p", "p", "p", "p", "p"},
+                {" ", " ", " ", " ", " ", " ", " ", " "},
+                {" ", " ", " ", " ", " ", " ", " ", " "},
+                {" ", " ", " ", " ", " ", " ", " ", " "},
+                {" ", " ", " ", " ", " ", " ", " ", " "},
+                {" ", " ", " ", "P", " ", " ", " ", " "},
+                {" ", " ", " ", " ", " ", " ", " ", " "}};
+        long[] boardArray = DebugBoard.arrayToBitboards(stringBoard, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L);
+        String expected = "63536343"; // could also be "63436353"
+        String actual = PossibleMoves.possibleMovesW(boardArray[0], boardArray[1], boardArray[2], boardArray[3],
+                boardArray[4], boardArray[5], boardArray[6], boardArray[7], boardArray[8], boardArray[9],
+                boardArray[10], boardArray[11], 0L, false, false, false, false);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void test05NoMoves() {
+        String[][] stringBoard = new String[][]{
+                {"r", "n", "b", "q", "k", "b", " ", "r"},
                 {"p", "p", "p", "p", "p", "p", " ", "p"},
                 {" ", " ", " ", " ", " ", " ", " ", " "},
                 {" ", " ", " ", " ", " ", " ", " ", " "},
+                {" ", " ", " ", "n", " ", " ", "p", " "},
+                {" ", " ", " ", "P", " ", " ", "P", " "},
                 {" ", " ", " ", " ", " ", " ", " ", " "},
-                {" ", " ", " ", " ", " ", " ", " ", "p"},
-                {"P", "P", "P", "P", "P", "P", "P", "P"},
-                {"R", "N", "B", "Q", "K", "B", "N", "R"}};
+                {" ", " ", " ", " ", " ", " ", " ", " "}};
         long[] boardArray = DebugBoard.arrayToBitboards(stringBoard, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L);
-        String expected = "6657";
+        String expected = "";
+        String actual = PossibleMoves.possibleMovesW(boardArray[0], boardArray[1], boardArray[2], boardArray[3],
+                boardArray[4], boardArray[5], boardArray[6], boardArray[7], boardArray[8], boardArray[9],
+                boardArray[10], boardArray[11], 0L, false, false, false, false);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void test06EnPassantRight() {
+        String[][] stringBoard = new String[][]{
+                {" ", " ", " ", " ", " ", " ", " ", " "},
+                {" ", " ", " ", " ", " ", " ", " ", " "},
+                {" ", " ", "r", " ", " ", " ", " ", " "},
+                {" ", " ", "P", "p", " ", " ", " ", " "},
+                {" ", " ", " ", " ", " ", " ", " ", " "},
+                {" ", " ", " ", " ", " ", " ", " ", " "},
+                {" ", " ", " ", " ", " ", " ", " ", " "},
+                {" ", " ", " ", " ", " ", " ", " ", " "}};
+        long[] boardArray = DebugBoard.arrayToBitboards(stringBoard, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L);
+        String expected = "23WE";
+        // make EP = WP since only one pawn on board
+        String actual = PossibleMoves.possibleMovesW(boardArray[0], boardArray[1], boardArray[2], boardArray[3],
+                boardArray[4], boardArray[5], boardArray[6], boardArray[7], boardArray[8], boardArray[9],
+                boardArray[10], boardArray[11], boardArray[6], false, false, false, false);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void test07EnPassantLeft() {
+        String[][] stringBoard = new String[][]{
+                {" ", " ", " ", " ", " ", " ", " ", " "},
+                {" ", " ", " ", " ", " ", " ", " ", " "},
+                {" ", " ", " ", " ", "r", " ", " ", " "},
+                {" ", " ", " ", "p", "P", " ", " ", " "},
+                {" ", " ", " ", " ", " ", " ", " ", " "},
+                {" ", " ", " ", " ", " ", " ", " ", " "},
+                {" ", " ", " ", " ", " ", " ", " ", " "},
+                {" ", " ", " ", " ", " ", " ", " ", " "}};
+        long[] boardArray = DebugBoard.arrayToBitboards(stringBoard, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L);
+        String expected = "43WE";
+        // make EP = WP since only one pawn on board
+        String actual = PossibleMoves.possibleMovesW(boardArray[0], boardArray[1], boardArray[2], boardArray[3],
+                boardArray[4], boardArray[5], boardArray[6], boardArray[7], boardArray[8], boardArray[9],
+                boardArray[10], boardArray[11], boardArray[6], false, false, false, false);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void test08PromoteStraight() {
+        String[][] stringBoard = new String[][]{
+                {" ", " ", " ", " ", " ", " ", " ", " "},
+                {" ", " ", "P", " ", " ", " ", " ", " "},
+                {" ", " ", " ", " ", " ", " ", " ", " "},
+                {" ", " ", " ", " ", " ", " ", " ", " "},
+                {" ", " ", " ", " ", " ", " ", " ", " "},
+                {" ", " ", " ", " ", " ", " ", " ", " "},
+                {" ", " ", " ", " ", " ", " ", " ", " "},
+                {" ", " ", " ", " ", " ", " ", " ", " "}};
+        long[] boardArray = DebugBoard.arrayToBitboards(stringBoard, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L);
+        String expected = "22QP22RP22BP22NP";
+        // make EP = WP since only one pawn on board
+        String actual = PossibleMoves.possibleMovesW(boardArray[0], boardArray[1], boardArray[2], boardArray[3],
+                boardArray[4], boardArray[5], boardArray[6], boardArray[7], boardArray[8], boardArray[9],
+                boardArray[10], boardArray[11], 0L, false, false, false, false);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void test08PromoteRight() {
+        String[][] stringBoard = new String[][]{
+                {" ", " ", "r", "b", " ", " ", " ", " "},
+                {" ", " ", "P", " ", " ", " ", " ", " "},
+                {" ", " ", " ", " ", " ", " ", " ", " "},
+                {" ", " ", " ", " ", " ", " ", " ", " "},
+                {" ", " ", " ", " ", " ", " ", " ", " "},
+                {" ", " ", " ", " ", " ", " ", " ", " "},
+                {" ", " ", " ", " ", " ", " ", " ", " "},
+                {" ", " ", " ", " ", " ", " ", " ", " "}};
+        long[] boardArray = DebugBoard.arrayToBitboards(stringBoard, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L);
+        String expected = "23QP23RP23BP23NP";
+        // make EP = WP since only one pawn on board
+        String actual = PossibleMoves.possibleMovesW(boardArray[0], boardArray[1], boardArray[2], boardArray[3],
+                boardArray[4], boardArray[5], boardArray[6], boardArray[7], boardArray[8], boardArray[9],
+                boardArray[10], boardArray[11], 0L, false, false, false, false);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void test08PromoteLeft() {
+        String[][] stringBoard = new String[][]{
+                {" ", " ", " ", " ", " ", " ", "n", "k"},
+                {" ", " ", " ", " ", " ", " ", " ", "P"},
+                {" ", " ", " ", " ", " ", " ", " ", " "},
+                {" ", " ", " ", " ", " ", " ", " ", " "},
+                {" ", " ", " ", " ", " ", " ", " ", " "},
+                {" ", " ", " ", " ", " ", " ", " ", " "},
+                {" ", " ", " ", " ", " ", " ", " ", " "},
+                {" ", " ", " ", " ", " ", " ", " ", " "}};
+        long[] boardArray = DebugBoard.arrayToBitboards(stringBoard, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L);
+        String expected = "76QP76RP76BP76NP";
+        // make EP = WP since only one pawn on board
         String actual = PossibleMoves.possibleMovesW(boardArray[0], boardArray[1], boardArray[2], boardArray[3],
                 boardArray[4], boardArray[5], boardArray[6], boardArray[7], boardArray[8], boardArray[9],
                 boardArray[10], boardArray[11], 0L, false, false, false, false);
