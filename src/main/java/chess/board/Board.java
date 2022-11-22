a6633039980c51fcc3971e79b083fee4ab63c912
Divy Patel <82171007+divy-07@users.noreply.github.com>
2022-11-21 22:47:11 -0800

    added debug board + checkstyle

--- src/main/java/chess/board/Board.java
+++ src/main/java/chess/board/Board.java
@@ -18,7 +18,7 @@ public class Board {
      *
      * @param fenString the new board state in FEN notation
      *                  (<a href="https://en.wikipedia.org/wiki/Forsyth%E2%80%93Edwards_Notation">format specifications</a>)
-     *                  TODO: add impacts on the engine/frame conditions
+     *                  // TODO: add impacts on the engine/frame conditions
      */
     public static void importFEN(String fenString) {
         return; // TODO: implement this method
@@ -29,31 +29,31 @@ public class Board {
      * Mostly for debugging purposes, but can be used for manual testing.
      *
      * @param wp, wn, wb, wr, wq, wk, bp, bn, bb, br, bq, bk bitboards of the chess pieces.
-     *                                                       Must be valid bitboards.
+     *            Must be valid bitboards.
      */
-    public static void drawBoard(long wp,long wn,long wb,long wr,long wq,long wk,long bp,long bn,long bb,long br,long bq,long bk) {
+    public static void drawBoard(long wp, long wn, long wb, long wr, long wq, long wk, long bp, long bn, long bb, long br, long bq, long bk) {
         /*
          * Convert the bitboards to a 2D array of 1-char strings.
          * Add pieces using bit comparisons.
          * And print it to the terminal.
          */
-        String[][] chessBoard =new String[8][8];
+        String[][] chessBoard = new String[8][8];
         for (int i = 0; i < 64; i++) {
-            chessBoard[i/8][i%8] = " ";
+            chessBoard[i / 8][i % 8] = " ";
         }
         for (int i = 0; i < 64; i++) {
-            if (((wp>>i)&1) == 1) chessBoard[i/8][i%8] = "P";
-            if (((wn>>i)&1) == 1) chessBoard[i/8][i%8] = "N";
-            if (((wb>>i)&1) == 1) chessBoard[i/8][i%8] = "B";
-            if (((wr>>i)&1) == 1) chessBoard[i/8][i%8] = "R";
-            if (((wq>>i)&1) == 1) chessBoard[i/8][i%8] = "Q";
-            if (((wk>>i)&1) == 1) chessBoard[i/8][i%8] = "K";
-            if (((bp>>i)&1) == 1) chessBoard[i/8][i%8] = "p";
-            if (((bn>>i)&1) == 1) chessBoard[i/8][i%8] = "n";
-            if (((bb>>i)&1) == 1) chessBoard[i/8][i%8] = "b";
-            if (((br>>i)&1) == 1) chessBoard[i/8][i%8] = "r";
-            if (((bq>>i)&1) == 1) chessBoard[i/8][i%8] = "q";
-            if (((bk>>i)&1) == 1) chessBoard[i/8][i%8] = "k";
+            if (((wp >> i) & 1) == 1) chessBoard[i / 8][i % 8] = "P";
+            if (((wn >> i) & 1) == 1) chessBoard[i / 8][i % 8] = "N";
+            if (((wb >> i) & 1) == 1) chessBoard[i / 8][i % 8] = "B";
+            if (((wr >> i) & 1) == 1) chessBoard[i / 8][i % 8] = "R";
+            if (((wq >> i) & 1) == 1) chessBoard[i / 8][i % 8] = "Q";
+            if (((wk >> i) & 1) == 1) chessBoard[i / 8][i % 8] = "K";
+            if (((bp >> i) & 1) == 1) chessBoard[i / 8][i % 8] = "p";
+            if (((bn >> i) & 1) == 1) chessBoard[i / 8][i % 8] = "n";
+            if (((bb >> i) & 1) == 1) chessBoard[i / 8][i % 8] = "b";
+            if (((br >> i) & 1) == 1) chessBoard[i / 8][i % 8] = "r";
+            if (((bq >> i) & 1) == 1) chessBoard[i / 8][i % 8] = "q";
+            if (((bk >> i) & 1) == 1) chessBoard[i / 8][i % 8] = "k";
         }
         for (int i = 0; i < 8; i++) {
             System.out.println(Arrays.toString(chessBoard[i]));
---
+++ src/main/java/chess/board/DebugBoard.java
@@ -0,0 +1,63 @@
+package chess.board;
+
+// Credit: Logic Crazy Chess
+public class DebugBoard {
+
+    public static void main(String[] args) {
+        long wp = 0L, wn = 0L, wb = 0L, wr = 0L, wq = 0L, wk = 0L, bp = 0L, bn = 0L, bb = 0L, br = 0L, bq = 0L, bk = 0L;
+        String[][] stringBoard = get2dStringBoard();
+        long[] boardArray = arrayToBitboards(stringBoard, wp, wn, wb, wr, wq, wk, bp, bn, bb, br, bq, bk);
+        drayArray(boardArray);
+    }
+
+    public static String[][] get2dStringBoard() {
+        // edit this method to change the starting board
+        String[][] chessBoard = {
+                {"r", "n", "b", "q", "k", "b", "n", "r"},
+                {"p", "p", "p", "p", "p", "p", "p", "p"},
+                {" ", " ", " ", " ", " ", " ", " ", " "},
+                {" ", " ", " ", " ", " ", " ", " ", " "},
+                {" ", " ", " ", " ", " ", " ", " ", " "},
+                {" ", " ", " ", " ", " ", " ", " ", " "},
+                {"P", "P", "P", "P", "P", "P", "P", "P"},
+                {"R", "N", "B", "Q", "K", "B", "N", "R"}};
+        return chessBoard;
+    }
+
+    public static void drayArray(long[] boardArray) {
+        Board.drawBoard(boardArray[0], boardArray[1], boardArray[2], boardArray[3], boardArray[4], boardArray[5],
+                boardArray[6], boardArray[7], boardArray[8], boardArray[9], boardArray[10], boardArray[11]);
+    }
+
+    public static long[] arrayToBitboards(String[][] chessBoard, long wp, long wn, long wb, long wr, long wq, long wk, long bp, long bn, long bb, long br, long bq, long bk) {
+        String binaryString;
+        for (int i = 0; i < 64; i++) {
+            binaryString = "0000000000000000000000000000000000000000000000000000000000000000";
+            binaryString = binaryString.substring(i + 1) + "1" + binaryString.substring(0, i);
+            switch (chessBoard[i / 8][i % 8]) {
+                case "P" -> wp += convertStringToBitboard(binaryString);
+                case "N" -> wn += convertStringToBitboard(binaryString);
+                case "B" -> wb += convertStringToBitboard(binaryString);
+                case "R" -> wr += convertStringToBitboard(binaryString);
+                case "Q" -> wq += convertStringToBitboard(binaryString);
+                case "K" -> wk += convertStringToBitboard(binaryString);
+                case "p" -> bp += convertStringToBitboard(binaryString);
+                case "n" -> bn += convertStringToBitboard(binaryString);
+                case "b" -> bb += convertStringToBitboard(binaryString);
+                case "r" -> br += convertStringToBitboard(binaryString);
+                case "q" -> bq += convertStringToBitboard(binaryString);
+                case "k" -> bk += convertStringToBitboard(binaryString);
+            }
+        }
+        return new long[]{wp, wn, wb, wr, wq, wk, bp, bn, bb, br, bq, bk};
+    }
+
+    public static long convertStringToBitboard(String binary) {
+        if (binary.charAt(0) == '0') {
+            //not going to be a negative number
+            return Long.parseLong(binary, 2);
+        } else {
+            return Long.parseLong("1" + binary.substring(2), 2) * 2;
+        }
+    }
+}
